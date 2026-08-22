967ecbc1c647cb6d2236bfe12d20389fc438316f
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-08-22 10:53:50 +0600

    Variables & Constants

---
+++ 4_variable.py
@@ -0,0 +1,43 @@
+# A variable is a name that refers to a value stored in memory 
+name = "Mehedi"
+age = 25 
+
+print(name)
+print(age)
+
+# Variable can change
+age = 26
+print(age)
+
+# Python doesn't require a data type declaration. 
+height = 5.11
+is_student = True
+
+print(type(height))
+print(type(is_student))
+
+# Different Types of Variables 
+age = 27 # integer 
+price = 28.5 # float 
+name = "John" # string 
+is_student = True # Boolean 
+fruits = ["Apple", "Banana", "Mango"] # List 
+
+# multiple variables 
+name, age, country = "Mehedi", 25, "Bangladesh"
+print(name)
+print(age)
+print(country)
+
+# Changing the data type 
+value = 10
+print(type(value))
+
+value = "Hello"
+print(type(value))
+
+# constants: a constant is a value that is intended to remainunchanged throughout a program 
+PI = 3.14159
+MAX_USERS = 100
+COUNTRY = "Bangladesh"
+# Python uses uppercase NAMES TO INDICATE CONSTANTS 
