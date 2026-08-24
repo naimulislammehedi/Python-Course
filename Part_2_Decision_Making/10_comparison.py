40033959ddd44e195300fa3efb5480db1927368d
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-08-24 12:10:08 +0600

    Comparison Operators

---
+++ Part_2_Decision_Making/10_comparison.py
@@ -0,0 +1,9 @@
+# Comparison (Relational) Operators
+# These operators compare two values and return True or False.
+
+x = 10
+y = 20
+
+print(x == y)
+print(x < y)
+print(x >= 10)
---
+++ Part_2_Decision_Making/exercise_3.py
@@ -0,0 +1,9 @@
+# Write Python code that:
+# Creates score = 75
+# Checks whether score is greater than or equal to 50
+# Stores the result in passed
+# Prints passed
+
+score = 75 
+passed = score >= 50
+print(passed)
