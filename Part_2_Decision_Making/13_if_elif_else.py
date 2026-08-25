737969aa0ba5256cd973205c0f55f412e9688bba
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-08-25 09:06:22 +0600

    if...elif...else

---
+++ Part_2_Decision_Making/13_if_elif_else.py
@@ -0,0 +1,23 @@
+# if...elif...else
+# When you need to check multiple conditions, use if...elif...else.
+
+score = 75
+
+if score >= 80:
+    print("A")
+elif score >= 60:
+    print("B")
+else:
+    print("C")
+
+# Another example 
+temperature = 25
+
+if temperature >= 35:
+    print("Very hot")
+elif temperature >= 30:
+    print("Hot")
+elif temperature >= 20:
+    print("Normal")
+else:
+    print("Cold")
---
+++ Part_2_Decision_Making/exercise_8.py
@@ -0,0 +1,18 @@
+# Write a program using if...elif...else:
+# score = 65
+# Rules:
+    # 80 or more → "A"
+    # 60 or more → "B"
+    # 40 or more → "C"
+    # Otherwise → "F"
+
+score = 65
+
+if score >= 80:
+    print("A")
+elif score >= 60:
+    print("B")
+elif score >= 40:
+    print("C")
+else:
+    print("F")
