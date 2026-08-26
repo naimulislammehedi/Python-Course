0f751a13cae3b6984d6021d127f6ff54712d0dd7
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-08-26 10:33:50 +0600

    for Loop

---
+++ Part_3_Loops/18_for.py
@@ -0,0 +1,19 @@
+# A for loop is used to repeat a block of code for each item in a sequence or a specific number of times.
+
+# Think of it as:
+    # “For each item, do this.”
+
+for number in [1, 2, 3, 4, 5]:
+    print(number)
+
+# for loop with a string 
+name = "Mehedi"
+
+for letter in name: 
+    print(letter)
+
+# for loop with a list 
+fruits = ["Apple", "Banana", "Mango"]
+
+for fruit in fruits:
+    print(fruit)
