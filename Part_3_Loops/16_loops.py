62545f3dd1ef0ef4d6169974d0df3b7809da3e92
Naimul Islam Mehedi <mehedi.interface@gmail.com>
2026-08-26 10:15:30 +0600

    while loop

---
+++ Part_3_Loops/16_loops.py
@@ -0,0 +1,13 @@
+# A loop is a programming structure that allows us to repeat a block of code multiple times. Instead of writing the same code again and again, we can use a loop to automate the repetition.
+
+for number in range(1, 6):
+    print(number)
+
+
+# Loops are useful whenever we need to perform the same operation repeatedly.
+# For example, suppose we have a list of students:
+students = ["Rahim", "Karim", "Hasan", "Sakib"]
+
+for student in students:
+    print(student)
+
